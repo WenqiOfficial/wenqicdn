896a49e6fb20c5f27fc9b1660e7609f5c13fe461
WenqiOfficial <67701681+WenqiOfficial@users.noreply.github.com>
2020-07-21 01:41:21 +0800

    Create hititle.js

---
+++ js/hititle.js
@@ -0,0 +1,16 @@
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         document.title = '(*/ω＼*)竟然抛弃我了';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         document.title = '(*^▽^*)欢迎回来~' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });

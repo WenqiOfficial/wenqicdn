f9f4437a5d6a5a0ea3902588e430f6026f33db84
WenqiOfficial <2747297361@qq.com>
2024-12-06 01:19:02 +0800

    Update hititle.js

--- js/hititle.js
+++ js/hititle.js
@@ -1,19 +1,20 @@
 var OriginTitle = document.title;
 var titleTime;
-window.addEventListener('hashchange', function (event) {
-    OriginTitle = document.title;
+document.addEventListener('visibilitychange', function () {
+    if (OriginTitle != document.title) {
+        OriginTitle = document.title;
+        console.log('Title changed to'+ document.title);
+    }
+    if (document.hidden) {
+        /*$('[rel="icon"]').attr('href', "/img/trhx2.png");*/
+        document.title = '你去哪里了啊... ' + OriginTitle;
+        clearTimeout(titleTime);
+    }
+    else {
+        /*$('[rel="icon"]').attr('href', "/img/trhx2.png");*/
+        document.title = '(*^▽^*)欢迎回来~' + OriginTitle;
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 1500);
+    }
 });
- document.addEventListener('visibilitychange', function () {
-     if (document.hidden) {
-         /*$('[rel="icon"]').attr('href', "/img/trhx2.png");*/
-         document.title = '你去哪里了啊...|'+ OriginTitle;
-         clearTimeout(titleTime);
-     }
-     else {
-         /*$('[rel="icon"]').attr('href', "/img/trhx2.png");*/
-         document.title = '(*^▽^*)欢迎回来~' + OriginTitle;
-         titleTime = setTimeout(function () {
-             document.title = OriginTitle;
-         }, 1500);
-     }
- });
